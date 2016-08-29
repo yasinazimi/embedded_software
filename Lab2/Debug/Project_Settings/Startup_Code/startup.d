df1234c0a0258723451b109a01c9cb2fa8c509eb
Yasin Azimi <Yasin Azimi>
2016-08-29 14:43:15 +1000

    Lab1 - Complete

--- Lab2/Debug/Project_Settings/Startup_Code/startup.d
+++
@@ -1,8 +0,0 @@
-Project_Settings/Startup_Code/startup.o: \
- ../Project_Settings/Startup_Code/startup.c \
- D:/UTS/Year\ 3/Spring\ Semester/48434\ -\ Embedded\ Software/SVN/embedded_software/Lab2/Generated_Code/IO_Map.h \
- D:/UTS/Year\ 3/Spring\ Semester/48434\ -\ Embedded\ Software/SVN/embedded_software/Lab2/Static_Code/IO_Map/MK70F12.h
-
-D:/UTS/Year\ 3/Spring\ Semester/48434\ -\ Embedded\ Software/SVN/embedded_software/Lab2/Generated_Code/IO_Map.h:
-
-D:/UTS/Year\ 3/Spring\ Semester/48434\ -\ Embedded\ Software/SVN/embedded_software/Lab2/Static_Code/IO_Map/MK70F12.h:
--- Lab2/Debug/Sources/LEDs.d
+++
@@ -1,8 +0,0 @@
-Sources/LEDs.o: ../Sources/LEDs.c ../Sources/LEDs.h ../Sources/types.h \
- D:/UTS/Year\ 3/Spring\ Semester/48434\ -\ Embedded\ Software/SVN/embedded_software/Lab2/Static_Code/IO_Map/MK70F12.h
-
-../Sources/LEDs.h:
-
-../Sources/types.h:
-
-D:/UTS/Year\ 3/Spring\ Semester/48434\ -\ Embedded\ Software/SVN/embedded_software/Lab2/Static_Code/IO_Map/MK70F12.h:
